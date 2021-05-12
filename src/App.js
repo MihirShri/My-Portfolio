a3af54bf9bbad0cb252685924e74b4cd8402a7d6
Mihir Shri <coe18b064@iiitdm.ac.in>
2021-05-12 19:40:34 +0530

    Added contact me form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Particles from "react-particles-js";
 import Navbar from "./components/Navbar";
@@ -6,6 +6,8 @@ import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
 import Experience from "./components/Experience";
+import Portfolio from "./components/Portfolio";
+import Contacts from "./components/Contacts";
 
 function App() {
   return (
@@ -18,17 +20,17 @@ function App() {
               value: 30,
               density: {
                 enable: true,
-                value_area: 900,
-              },
+                value_area: 900
+              }
             },
             shape: {
               type: "circle",
               stroke: {
                 width: 6,
-                color: "#f9ab00",
-              },
-            },
-          },
+                color: "#f9ab00"
+              }
+            }
+          }
         }}
       />
       <Navbar />
@@ -36,6 +38,8 @@ function App() {
       <AboutMe />
       <Services />
       <Experience />
+      <Portfolio />
+      <Contacts />
     </>
   );
 }
